ef3ad28a35da99debd4d8e370b249a2726aad312
BoZhang-dev <boruiprofessionnel@outlook.com>
2025-04-11 16:55:54 +0200

    sync

--- Codes/Arduino-Borui/Arduino-Borui.ino
+++ Codes/Arduino-Borui/Arduino-Borui.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <rgb_lcd.h>
+
+rgb_lcd lcd;
+float colorR;
+float colorG;
+float colorB;
+float Score;
+
+void setup(){
+  lcd.begin(16, 2);
+  colorR = 182;
+  colorG = 240;
+  colorB = 252;
+  lcd.display();
+  lcd.setRGB(colorR, colorG, colorB);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Bonjour !");
+  delay(1000);
+  lcd.clear();
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Chargement");
+  lcd.setCursor(0, 1);
+  lcd.print("du systeme");
+  delay(3000);
+
+  
+  Score = 0;
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+void loop(){
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Score :");
+  lcd.setCursor(0, 1);
+  lcd.print(Score);
+  delay(5000);
 
 }
